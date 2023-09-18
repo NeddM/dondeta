4bd4b4ac769e04436ffae8bcb20d296c07c838b9
Nedd Chairi Muñoz <neddchairiweb@gmail.com>
2023-09-18 21:29:19 +0200

    📚 Readme added and print small fix

---
+++ README.md
@@ -0,0 +1,43 @@
+# Dondeta
+
+Find a file on every folder under the directory that you are looking for. Usefull to find files that you remember the name but you forgot where you saved it
+
+## Prerequisites
+
+Before using this script, ensure that you have the following prerequisites installed on your system:
+
+-   Rust: You can install Rust by following the instructions on the official website: Rust Installation.
+-   Git: Make sure Git is installed on your system and configured with the repository where you want to make commits.
+
+## How to Use
+
+Follow these steps to use the "dondeta" script:
+
+1. Clone or navigate where you want to use dondeta.
+
+2. Save the "dondeta" Rust script to a file, e.g., dondeta.rs.
+
+3. Compile the Rust script by running the following command:
+
+```shell
+cargo build --release
+```
+
+4. Save the exe on `System32` in Windows or `/usr/local/bin/` in Linux.
+
+
+5. Run the compiled executable:
+
+```shell
+dondeta
+```
+
+6. Follow the on-screen prompts:
+
+```shell
+dondeta -h
+```
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,24 @@ fn main() {
         println!("Eg: dondeta [file-name] [folder-search-from]");
     } else if args.len() == 2 {
         let file_to_find = args[1].as_str();
-        treeing(&path_source, file_to_find);
+        find_file(&path_source, file_to_find);
     } else if args.len() == 3 {
         let file_to_find = args[1].as_str();
         let path_source = args[2].as_str();
-        treeing(&path_source, file_to_find);
+        find_file(&path_source, file_to_find);
     } else {
-        println!("Insert a valid argument.")
+        println!("Insert valid arguments.")
     }
 }
 
-fn treeing(new_dir: &str, file_to_find: &str) {
+fn find_file(new_dir: &str, file_to_find: &str) {
     match fs::read_dir(new_dir) {
         Ok(data) => {
             for entry in data {
                 match entry {
                     Ok(entry) => {
                         if entry.path().is_dir() {
-                            treeing(entry.path().to_str().unwrap(), file_to_find);
+                            find_file(entry.path().to_str().unwrap(), file_to_find);
                         } else if entry.path().is_file() && entry.file_name() == file_to_find {
                             println!("{}", entry.path().to_str().unwrap());
                         }
